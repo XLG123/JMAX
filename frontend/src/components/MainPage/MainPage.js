1e2c1fdc184343e7e37664a4e80815a85595fab7
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-08-06 11:21:33 -0400

    Initialized Frontend

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+const MainPage = () => {
+  return (
+    <>
+      <p>A Twitter Clone</p>
+      <footer>
+        Copyright &copy; 2022 Chirper
+      </footer>
+    </>
+  );
+}
+
+export default MainPage;
---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './components/store/store';
+
+let store = configureStore({});
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
