d093bd59c9900a00b98376ace26b8990424a6956
Manel-Os <oulds.manel@gmail.com>
2023-08-06 10:02:34 -0400

    current user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const passport = require("passport");
+const { loginUser, restoreUser } = require("../../config/passport");
+const { isProduction } = require("../../config/keys");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -20,7 +22,7 @@ router.post("/register", async (req, res, next) => {
   });
 
   if (user) {
-    // Throw a 400 error if the email address and/or email already exists
+    // Throw a 400 error if the email address and/or username already exists
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
@@ -49,7 +51,7 @@ router.post("/register", async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       } catch (err) {
         next(err);
       }
@@ -66,8 +68,24 @@ router.post("/login", async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+router.get("/current", restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email,
+  });
 });
 
 module.exports = router;
