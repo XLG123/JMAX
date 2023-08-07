c1ce5ac7716cbd28f40985f31f8124bded829a9e
Manel-Os <oulds.manel@gmail.com>
2023-08-07 12:11:38 -0400

    Deleting

--- backend/routes/api/problems.js
+++ backend/routes/api/problems.js
@@ -44,13 +44,45 @@ router.get("/:id", requireUser, async (req, res, next) => {
   }
 });
 
-router.delete("/:id", requireUser, async (req, res, next) => {
+// router.delete("/:id", requireUser, async (req, res, next) => {
+//   try {
+//     const problem = await Problem.findById(req.params.id)
+//       .populate("author", "_id username email")
+//       .then((problem) => problem.remove());
+//   } catch (err) {
+//     const error = new Error("Error deleting problem");
+//     error.statusCode = 404;
+//     error.errors = { message: "Error deleting problem" };
+//     return next(error);
+//   }
+// });
+
+router.delete("/:id", requireUser, async (req, res) => {
   try {
-    const problem = await Problem.findById(req.params.id)
-      .populate("author", "_id username email")
-      .then((problem) => problem.remove());
-  } catch (err) {
-    const error = new Error("Problem not found");
+    // Find the problem by ID
+    const problem = await Problem.findById(req.params.id);
+    console.log(problem);
+    console.log(problem.author_id);
+
+    // Check if the problem exists
+    if (!problem) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
+
+    // Check if the user is the author of the problem
+    if (problem.author_id.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this problem" });
+    }
+
+    // Delete the problem
+    await Problem.deleteOne({ _id: problem._id });
+
+    return res.json({ message: "Problem deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error deleting problem" });
   }
 });
 
