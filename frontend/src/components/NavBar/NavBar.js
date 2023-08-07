be13baa1f5339c323fd3bd834b259082a23219dc
Jordy <jordycorporan95@gmail.com>
2023-08-07 07:04:42 -0400

    Demo user fully functioning

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,50 +1,45 @@
-import { NavLink, useHistory } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import './NavBar.css';
-import { logout, login } from '../store/session';
+import { NavLink, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import "./NavBar.css";
+import { logout, login } from "../store/session";
 
-import webAppLogo from '../../assets/images/webAppLogo.jpg';
+import webAppLogo from "../../assets/images/webAppLogo.jpg";
 
 function NavBar() {
-  const loggedIn = useSelector(state => !!state.session.user);
+  const loggedIn = useSelector((state) => !!state.session.user);
   const dispatch = useDispatch();
   let history = useHistory();
 
-  const logoutUser = e => {
+  const logoutUser = (e) => {
     e.preventDefault();
     dispatch(logout());
-  }
-
+  };
 
   const demoLogin = () => {
     const demoInformation = {
       email: "DEMO-USER@email.com",
-      password: "password"
+      password: "password",
     };
     dispatch(login(demoInformation))
-    .then(() => {
-      history.push("/home");
-    })
-    .catch((error) => {
-      console.error("Error logging in as demo user:", error);
-    });
-
-
-
-
+      .then(() => {
+        history.push("/home");
+      })
+      .catch((error) => {
+        console.error("Error logging in as demo user:", error);
+      });
   };
 
   const goToAbout = () => {
-    history.push('/about');
-  }
+    history.push("/about");
+  };
 
   const getLinks = () => {
     if (loggedIn) {
       return (
         <div className="links-nav">
-          <NavLink to='/tweets'>All Tweets</NavLink>
-          <NavLink to='/profile'>Profile</NavLink>
-          <NavLink to='/tweets/new'>Write a Tweet</NavLink>
+          <NavLink to="/tweets">All Tweets</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/tweets/new">Write a Tweet</NavLink>
           <button onClick={logoutUser}>Logout</button>
         </div>
       );
@@ -52,49 +47,57 @@ function NavBar() {
       return (
         <>
           <div className="links-auth">
-
-            <div className='nav-btn' id="demo-login" onClick={demoLogin}>Demo
-              <span></span><span></span><span></span><span></span>
+            <div className="nav-btn" id="demo-login" onClick={demoLogin}>
+              Demo
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
               {/* the empty spans are for css styling effects */}
             </div>
 
-            <NavLink to='/signup' className="nav-btn">
+            <NavLink to="/signup" className="nav-btn">
               Sign Up
-              <span></span><span></span><span></span><span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
               {/* the empty spans are for css styling effects */}
             </NavLink>
 
-            <NavLink to='/login' className="nav-btn">
+            <NavLink to="/login" className="nav-btn">
               Log In
-              <span></span><span></span><span></span><span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
               {/* the empty spans are for css styling effects */}
             </NavLink>
-
           </div>
         </>
       );
     }
-  }
+  };
 
   return (
     <>
-      <div className='nav-bar-container'>
-
-        <NavLink to='/'>
-          <div className='logo-container'>
-            <img src={webAppLogo} alt='app-logo' className='main-pg-logo'/>
+      <div className="nav-bar-container">
+        <NavLink to="/">
+          <div className="logo-container">
+            <img src={webAppLogo} alt="app-logo" className="main-pg-logo" />
           </div>
         </NavLink>
 
-        <div className="nav-btn" id="about-btn" onClick=
-        {goToAbout}>
+        <div className="nav-btn" id="about-btn" onClick={goToAbout}>
           About
-          <span></span><span></span><span></span><span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           {/* the empty spans are for css styling effects */}
         </div>
 
         {getLinks()}
-
       </div>
     </>
   );
