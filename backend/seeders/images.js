e8215b24c2c25aeb475769e8c88424a0134a3659
Jordy <jordycorporan95@gmail.com>
2023-08-08 09:44:32 -0400

    Bug is still present

--- backend/seeders/images.js
+++ backend/seeders/images.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
-
-// const User = require('../models/User');
 const Problem = require('../models/Problem.js');
 
 const DEFAULT_PROBLEM_IMAGE_URL = 'https://my-jmax.s3.us-east-2.amazonaws.com/public/tools.svg'; // <- Insert the S3 URL that you copied above here
@@ -19,11 +17,16 @@ mongoose
     process.exit(1);
   });
 
-// Initialize image fields in db
-const initializeImages = async () => {
-  console.log("Initializing problem avatars...");
-  await Problem.updateMany({}, { problemImageUrl: DEFAULT_PROBLEM_IMAGE_URL });
 
-  console.log("Done!");
-  mongoose.disconnect();
-}
+  // Initialize image fields in db
+  const initializeImages = async () => {
+    console.log("Initializing problem avatars...");
+    await Problem.updateMany({}, { problemImageUrl: DEFAULT_PROBLEM_IMAGE_URL });
+
+    console.log("Done!");
+    mongoose.disconnect();
+  }
+
+  module.exports = {
+    DEFAULT_PROBLEM_IMAGE_URL,
+  };
