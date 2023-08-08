25aaf2a3534a825528265f510759d82830156ab8
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-08-08 19:37:57 -0400

    render

--- backend/app.js
+++ backend/app.js
@@ -53,6 +53,28 @@ app.use("/api/offers", offersRouter);
 app.use("/api/csrf", csrfRouter);
 app.use("/api/test", testUserRouter);
 
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 app.use((req, res, next) => {
   const err = new Error("Not Found");
   err.statusCode = 404;
