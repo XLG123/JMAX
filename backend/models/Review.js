ed798bd22fb3d6443ae1462d8e27c21a9b73d9e4
Jordy <jordycorporan95@gmail.com>
2023-08-08 22:14:55 -0400

    Add rating column

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema(
-  
-);
+const reviewSchema = new Schema({
+    rating: { type: Number, min: 1, max: 5 }
+
+});
