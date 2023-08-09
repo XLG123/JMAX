740c38787a5e434e490b5c71805cf79959ad6811
Jordy <jordycorporan95@gmail.com>
2023-08-08 22:19:55 -0400

    Update Review Model - Add body column - Add reviewer column - Add reviewee column

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-  rating: { type: Number, min: 1, max: 5 },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
   body: {
     type: String,
+  },
+  reviewer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviewee: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
   },
 });
