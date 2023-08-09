56936f39c6b490c2e2035dd9eb1e8f35adda86a9
Jordy <jordycorporan95@gmail.com>
2023-08-08 22:29:01 -0400

    Update user model for reviews

--- backend/models/User.js
+++ backend/models/User.js
@@ -23,13 +23,15 @@ const userSchema = new Schema(
       type: Number,
       required: true,
     },
+    reviewsWritten: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+    reviewsReceived: [{ type: Schema.Types.ObjectId, ref: "Review" }],
   },
   {
     timestamps: true,
   }
 );
 
-userSchema.methods.toSafeObject = function() {
+userSchema.methods.toSafeObject = function () {
   let user = this.toObject();
   delete user.hashedPassword;
   return user;
@@ -37,14 +39,16 @@ userSchema.methods.toSafeObject = function() {
 // userSchema.methods.getProblems = function() {
 //   return mongoose.model('Problem').find({ author: this._id }).select('_id').populate();
 // };
-userSchema.methods.getProblems = async function() {
-  const problems = await mongoose.model('Problem').find({ author: this._id }).select('_id');
-  return problems.map(problem => problem._id);
+userSchema.methods.getProblems = async function () {
+  const problems = await mongoose
+    .model("Problem")
+    .find({ author: this._id })
+    .select("_id");
+  return problems.map((problem) => problem._id);
 };
 // userSchema.virtual('birthdateFormatted').get(function () {
 //   return moment(this.birthdate).format('MM-DD-YYYY');
 // });
-
 
 // userSchema.set('toJSON', { virtuals: true });
 // userSchema.set('toObject', { virtuals: true });
