1fdb5bf8f39fa409695c2f87c07135a329b2a8b1
Jordy <jordycorporan95@gmail.com>
2023-08-10 04:08:29 -0400

    Added message model

---
+++ backend/models/Message.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+module.exports = Message;
