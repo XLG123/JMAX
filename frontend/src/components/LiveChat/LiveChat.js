f86744a35390a13234f82a5eabe95eeb5815e0e4
Jordy <jordycorporan95@gmail.com>
2023-08-09 23:14:44 -0400

    Set up port for front end

--- frontend/src/components/LiveChat/LiveChat.js
+++ frontend/src/components/LiveChat/LiveChat.js
@@ -6,4 +6,20 @@ let socket;
 const LivePrivateChat = () => {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket = io("http://localhost:4000");
+
+    socket.on("chat message", (msg) => {
+      setChat([...chat, msg]);
+    });
+
+    return () => socket.disconnect();
+  }, [chat]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("chat message", message);
+    setMessage("");
+  };
 };
