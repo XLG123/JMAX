4b6859451070bc4bfeb54e73940052ef68811435
Jordy <jordycorporan95@gmail.com>
2023-08-09 23:09:01 -0400

    Add state for messages

--- frontend/src/components/LiveChat/LiveChat.js
+++ frontend/src/components/LiveChat/LiveChat.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 let socket;
 
 const LivePrivateChat = () => {
-
-}
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+};
