0ce37b308d5562f4e24dc0aa7c421e3f41f6bba7
Manel-Oulds <oulds.manel@gmail.com>
2023-08-22 11:41:34 -0400

    Edit problem with image working

--- frontend/src/components/store/problems.js
+++ frontend/src/components/store/problems.js
@@ -1,58 +1,57 @@
-import jwtFetch from './jwt';
-import * as offerActions from "./offers"
-import { RECEIVE_USER_LOGOUT } from './session';
+import jwtFetch from "./jwt";
+import * as offerActions from "./offers";
+import { RECEIVE_USER_LOGOUT } from "./session";
 const RECEIVE_PROBLEMS = "problems/RECEIVE_PROBLEMS";
 const RECEIVE_PROBLEM = "problems/RECEIVE_PROBLEM";
 
 const RECEIVE_USER_PROBLEMS = "problems/RECEIVE_USER_PROBLEMS";
 const RECEIVE_NEW_PROBLEM = "problems/RECEIVE_NEW_PROBLEM";
-export const REMOVE_PROBLEM = "problems/REMOVE_PROBLEM"; 
+export const REMOVE_PROBLEM = "problems/REMOVE_PROBLEM";
 const RECEIVE_PROBLEM_ERRORS = "problems/RECEIVE_PROBLEM_ERRORS";
 const CLEAR_PROBLEM_ERRORS = "problems/CLEAR_PROBLEM_ERRORS";
-const UPDATE_PROBLEM= "problems/UPDATE_PROBLEM"
-const RECEIVE_USER_PROBLEMS_OPEN="problems/RECEIVE_USER_PROBLEMS_OPEN"
+const UPDATE_PROBLEM = "problems/UPDATE_PROBLEM";
+const RECEIVE_USER_PROBLEMS_OPEN = "problems/RECEIVE_USER_PROBLEMS_OPEN";
 const updateProblem = (id, updatedProblem) => ({
   type: UPDATE_PROBLEM,
   id,
-  updatedProblem
+  updatedProblem,
 });
-const receiveProblem = problem => ({
+const receiveProblem = (problem) => ({
   type: RECEIVE_PROBLEM,
-  problem
+  problem,
 });
-const receiveProblems = problems => ({
+const receiveProblems = (problems) => ({
   type: RECEIVE_PROBLEMS,
-  problems
+  problems,
 });
 
-const receiveUserProblems = problems => ({
+const receiveUserProblems = (problems) => ({
   type: RECEIVE_USER_PROBLEMS,
-  problems
+  problems,
 });
-const receiveUserProblemsOpen = problems => ({
+const receiveUserProblemsOpen = (problems) => ({
   type: RECEIVE_USER_PROBLEMS_OPEN,
-  problems
+  problems,
 });
 
-const receiveNewProblem = problem => ({
+const receiveNewProblem = (problem) => ({
   type: RECEIVE_NEW_PROBLEM,
-  problem
+  problem,
 });
 
-const receiveErrors = errors => ({
+const receiveErrors = (errors) => ({
   type: RECEIVE_PROBLEM_ERRORS,
-  errors
+  errors,
 });
 
-export const clearProblemErrors = errors => ({
+export const clearProblemErrors = (errors) => ({
   type: CLEAR_PROBLEM_ERRORS,
-  errors
+  errors,
 });
 
-export const fetchProblems = () => async dispatch => {
-
+export const fetchProblems = () => async (dispatch) => {
   try {
-    const res = await jwtFetch('/api/problems');
+    const res = await jwtFetch("/api/problems");
     const problems = await res.json();
     dispatch(receiveProblems(problems));
   } catch (err) {
@@ -63,7 +62,7 @@ export const fetchProblems = () => async dispatch => {
   }
 };
 
-export const fetchUserProblems = id => async dispatch => {
+export const fetchUserProblems = (id) => async (dispatch) => {
   try {
     const res = await jwtFetch(`/api/users/${id}/problems`);
     const problems = await res.json();
@@ -76,7 +75,7 @@ export const fetchUserProblems = id => async dispatch => {
   }
 };
 
-export const fetchProblem = id => async dispatch => {
+export const fetchProblem = (id) => async (dispatch) => {
   try {
     const res = await jwtFetch(`/api/problems/${id}`);
     const problem = await res.json();
@@ -88,7 +87,7 @@ export const fetchProblem = id => async dispatch => {
     }
   }
 };
-export const fetchUserProblemsOpen = id => async dispatch => {
+export const fetchUserProblemsOpen = (id) => async (dispatch) => {
   try {
     const res = await jwtFetch(`/api/users/${id}/problems/open`);
     const problems = await res.json();
@@ -103,27 +102,34 @@ export const fetchUserProblemsOpen = id => async dispatch => {
 
 export const deleteProblem = (id) => async (dispatch, getState) => {
   await jwtFetch(`/api/problems/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
 
-  const user = getState().session.user
+  const user = getState().session.user;
   dispatch({
     type: REMOVE_PROBLEM,
     problemId: id,
   });
 
-  dispatch(fetchUserProblems(user._id))
-}
+  dispatch(fetchUserProblems(user._id));
+};
 
-export const composeProblem = data => async dispatch => {
+export const composeProblem = (data) => async (dispatch) => {
+  const { image, description, category, address } = data;
+  const formData = new FormData();
+  formData.append("description", description);
+  formData.append("category", category);
+  formData.append("address", address);
+
+  if (image) formData.append("image", image);
   try {
-    const res = await jwtFetch('/api/problems/create', {
-      method: 'POST',
-      body: JSON.stringify(data)
+    const res = await jwtFetch("/api/problems/create", {
+      method: "POST",
+      body: formData,
     });
     const problem = await res.json();
     dispatch(receiveNewProblem(problem));
-    dispatch(fetchProblems())
+    dispatch(fetchProblems());
   } catch (err) {
     const resBody = await err.json();
     if (resBody.statusCode === 400) {
@@ -132,26 +138,54 @@ export const composeProblem = data => async dispatch => {
   }
 };
 
+// export const fetchUpdateProblem = (problemId, updatedData) => async (dispatch,getState) => {
+//   try {
+//     const res = await jwtFetch(`/api/problems/${problemId}`, {
+//       method: 'PATCH',
+//       body: JSON.stringify(updatedData)
+//     });
+//     const user=getState().session.user
+//     if (res.ok) {
+//       const updatedProblem = await res.json();
+//       dispatch(updateProblem(problemId, updatedProblem));
+//       dispatch(fetchUserProblems(user._id));
+//       dispatch(offerActions.fetchUserOffers(user._id))
+//       dispatch(fetchUserProblems(user._id));
+//     }
+//   } catch (err) {
+//     // Handle error
+//   }
+// };
 
-export const fetchUpdateProblem = (problemId, updatedData) => async (dispatch,getState) => {
-  try {
-    const res = await jwtFetch(`/api/problems/${problemId}`, {
-      method: 'PATCH',
-      body: JSON.stringify(updatedData)
+export const fetchUpdateProblem =
+  (problemId, updatedData) => async (dispatch, getState) => {
+    const { image, ...otherUpdatedData } = updatedData;
+
+    const formData = new FormData();
+    formData.append("image", image);
+    Object.keys(otherUpdatedData).forEach((key) => {
+      formData.append(key, otherUpdatedData[key]);
     });
-    const user=getState().session.user
-    if (res.ok) {
-      const updatedProblem = await res.json();
-      dispatch(updateProblem(problemId, updatedProblem));
-      dispatch(fetchUserProblems(user._id));
-      dispatch(offerActions.fetchUserOffers(user._id))
-      dispatch(fetchUserProblems(user._id));
-    }
-  } catch (err) {
-    // Handle error
-  }
-};
 
+    try {
+      const res = await jwtFetch(`/api/problems/${problemId}`, {
+        method: "PATCH",
+        body: formData,
+      });
+
+      const user = getState().session.user;
+
+      if (res.ok) {
+        const updatedProblem = await res.json();
+        dispatch(updateProblem(problemId, updatedProblem));
+        dispatch(fetchUserProblems(user._id));
+        dispatch(offerActions.fetchUserOffers(user._id));
+        dispatch(fetchUserProblems(user._id));
+      }
+    } catch (err) {
+      // Handle error
+    }
+  };
 
 const nullErrors = null;
 
@@ -167,11 +201,14 @@ export const problemErrorsReducer = (state = nullErrors, action) => {
   }
 };
 
-const problemsReducer = (state = { all: {}, user: [], new: undefined }, action) => {
+const problemsReducer = (
+  state = { all: {}, user: [], new: undefined },
+  action
+) => {
   switch (action.type) {
     case RECEIVE_PROBLEMS:
       return { ...state, all: action.problems, new: undefined };
-      case RECEIVE_PROBLEM:
+    case RECEIVE_PROBLEM:
       return { ...state, all: action.problem, new: undefined };
     case RECEIVE_USER_PROBLEMS:
       return { ...state, userProblems: action.problems, new: undefined };
@@ -182,13 +219,12 @@ const problemsReducer = (state = { all: {}, user: [], new: undefined }, action) 
       delete newState[action.problemId];
       return { ...newState };
     case RECEIVE_USER_LOGOUT:
-      return { ...state, user: [], new: undefined }
+      return { ...state, user: [], new: undefined };
     case UPDATE_PROBLEM:
       return { ...state, all: action.updatedProblem, new: undefined };
     default:
       return state;
   }
-
 };
 
 export default problemsReducer;
