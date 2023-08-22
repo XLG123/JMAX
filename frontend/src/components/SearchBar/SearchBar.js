3d1e3c087aaac8fb6ac4c82f4e30a8d08fbec089
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-08-22 18:34:03 -0400

    searchbar dropdown menu

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -7,7 +7,26 @@ import Tooltip from "@mui/material/Tooltip";
 const SearchBar = () => {
   const history = useHistory();
   const [searchText, setSearchText] = useState("");
-  const handleSearchSubmit = (e) => {
+
+  const dropdownSearch = (dropdownItem, e) => {
+    e.preventDefault();
+    if (isNaN(parseInt(dropdownItem[0]))) {
+      fetch(`/api/problems/search/category/${dropdownItem}`)
+        .then((response) => response.json())
+        .then((data) => {
+          history.push("/search-results", { searchResults: data });
+        });
+    } else {
+      fetch(`/api/problems/search/address/${dropdownItem}`)
+        .then((response) => response.json())
+        .then((data) => {
+          history.push("/search-results", { searchResults: data });
+        });
+    }
+    setSearchText("");
+  };
+
+  const handleSearch = (e) => {
     e.preventDefault();
     const wordCount = searchText.split(" ").length;
     let caseInsensitive = "";
@@ -31,6 +50,10 @@ const SearchBar = () => {
     }
     // console.log(caseInsensitive);
 
+    if (caseInsensitive === "Repair Home") {
+      caseInsensitive = "Home Repair";
+    }
+
     if (isNaN(parseInt(searchText[0]))) {
       fetch(`/api/problems/search/category/${caseInsensitive}`)
         .then((response) => response.json())
@@ -50,10 +73,7 @@ const SearchBar = () => {
   return (
     <div className="search-bar-container">
       <Tooltip title="Search">
-        <form
-          className="search-bar-form"
-          onSubmit={(e) => handleSearchSubmit(e)}
-        >
+        <form className="search-bar-form" onSubmit={(e) => handleSearch(e)}>
           <input
             type="text"
             placeholder="Enter a zip code or a category name"
@@ -66,6 +86,108 @@ const SearchBar = () => {
           </button>
         </form>
       </Tooltip>
+      {searchText && (
+        <div className="search-dropdown-menu">
+          <ul>
+            {isNaN(parseInt(searchText[0])) ? (
+              searchText[0].toLowerCase() === "h" ? (
+                <li
+                  className="dropdown-item"
+                  onClick={(e) => dropdownSearch("Home Repair", e)}
+                >
+                  Home Repair
+                </li>
+              ) : searchText[0].toLowerCase() === "d" ? (
+                <>
+                  <li
+                    className="dropdown-item"
+                    onClick={(e) => dropdownSearch("Delivery", e)}
+                  >
+                    Delivery
+                  </li>
+                  <li
+                    className="dropdown-item"
+                    onClick={(e) => dropdownSearch("Driver", e)}
+                  >
+                    Driver
+                  </li>
+                </>
+              ) : (
+                <></>
+              )
+            ) : searchText[0] === "1" ? (
+              <>
+                {searchText[1] === "0" ? (
+                  <li
+                    className="dropdown-item"
+                    onClick={(e) => dropdownSearch("10312", e)}
+                  >
+                    10312
+                  </li>
+                ) : (
+                  <></>
+                )}
+                {searchText[1] === "1" ? (
+                  <>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("11212", e)}
+                    >
+                      11212
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("11420", e)}
+                    >
+                      11420
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("11421", e)}
+                    >
+                      11421
+                    </li>
+                  </>
+                ) : (
+                  <></>
+                )}
+                {searchText[1] === "2" ? (
+                  <>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("12123", e)}
+                    >
+                      12123
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("12312", e)}
+                    >
+                      12312
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("12323", e)}
+                    >
+                      12323
+                    </li>
+                    <li
+                      className="dropdown-item"
+                      onClick={(e) => dropdownSearch("12345", e)}
+                    >
+                      12345
+                    </li>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </>
+            ) : (
+              <li className="dropdown-item"></li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
