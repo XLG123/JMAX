fd928c46e7817fb0afd9342aaf6b025f3d9214e3
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-08-26 13:06:59 -0400

    working on notify button modal styling

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -30,7 +30,7 @@ const Profile = () => {
   }
 
   const user = users[userId];
-  debugger
+  // debugger
   const [image, setImage] = useState(user?.profileImageUrl || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
  
 const [showEditProfile,setShowEditProfile]=useState()
--- frontend/src/components/offers/offerBox.js
+++ frontend/src/components/offers/offerBox.js
@@ -1,74 +1,85 @@
 // import "./ProblemBox.css"
-import * as problemActions from "../store/problems"
-import * as sessionActions from "../store/session"
-import * as offerActions from "../store/offers"
-import { useState } from 'react';
-import Modal from "../context/model"
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import "./offer.css"
-const OfferBox = ({ offer: {description ,price,status ,helper ,_id ,problem} }) => {
-const user= useSelector(state=>state.session.user)
-  const [show,setShow]=useState(false)
-  const history=useHistory()
-  const dispatch=useDispatch()
+import * as problemActions from "../store/problems";
+import * as sessionActions from "../store/session";
+import * as offerActions from "../store/offers";
+import { useState } from "react";
+import Modal from "../context/model";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import "./offer.css";
+const OfferBox = ({
+  offer: { description, price, status, helper, _id, problem },
+}) => {
+  const user = useSelector((state) => state.session.user);
+  const [show, setShow] = useState(false);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(sessionActions.fetchAllUsers())
-    dispatch(offerActions.fetchUserOffers(user._id))
-  },[dispatch])
+  useEffect(() => {
+    dispatch(sessionActions.fetchAllUsers());
+    dispatch(offerActions.fetchUserOffers(user._id));
+  }, [dispatch]);
 
-  const offerOwner= useSelector(state=> state.session.users)
-  function handelSubmit(e){
-    e.preventDefault()
+  const offerOwner = useSelector((state) => state.session.users);
+  function handelSubmit(e) {
+    e.preventDefault();
   }
   // console.log(offerOwner[helper].username)
-  function redirectToHelper(e){
-e.preventDefault()
-history.push(`/users/${helper}`)
+  function redirectToHelper(e) {
+    e.preventDefault();
+    history.push(`/users/${helper}`);
   }
 
-function handelDeleteOffer(e){
-e.preventDefault()
-dispatch(offerActions.fetchDeleteOffer(_id))
-}
+  function handelDeleteOffer(e) {
+    e.preventDefault();
+    dispatch(offerActions.fetchDeleteOffer(_id));
+  }
 
-function handelTakingOffer(e){
-e.preventDefault()
-dispatch(problemActions.fetchUpdateProblem(problem,{status: "closed"}))
-dispatch(offerActions.fetchUpdateOffer(_id, {status: "accepted"})).then(()=>{
-  history.push(`/users/${helper}`)
-})
+  function handelTakingOffer(e) {
+    e.preventDefault();
+    dispatch(problemActions.fetchUpdateProblem(problem, { status: "closed" }));
+    dispatch(offerActions.fetchUpdateOffer(_id, { status: "accepted" })).then(
+      () => {
+        history.push(`/users/${helper}`);
+      }
+    );
+  }
 
-}
-
-
-if (!offerOwner)return null
+  if (!offerOwner) return null;
 
   return (
     <>
-    <div className="offer-container">
-    <form onSubmit={handelTakingOffer}>
-    {/* <div className=""> */}
-      <h3 onClick={redirectToHelper} className="user bigger">{offerOwner[helper].username}</h3>
-<div className="close"> <FontAwesomeIcon icon={faTrashAlt} style={{ color: '#FF5733' }}  onClick={handelDeleteOffer}/></div>
+      <div className="offer-container offer-container-on-notify">
+        <form onSubmit={handelTakingOffer}>
+          {/* <div className=""> */}
+          <h3 onClick={redirectToHelper} className="user bigger">
+            {offerOwner[helper].username}
+          </h3>
+          <div className="close">
+            {" "}
+            <FontAwesomeIcon
+              icon={faTrashAlt}
+              style={{ color: "#FF5733" }}
+              onClick={handelDeleteOffer}
+            />
+          </div>
 
-      <p className="green">status : {status}</p>
+          <p className="green">status : {status}</p>
 
-      <p className="des-box">Price : {price}</p>
-      <p className="des-box">offer : {description}</p>
-    {/* </div> */}
-<button className="sign-up-btn pad " type="submit">Accept Offer</button>
-
-</form>
-</div>
-
+          <p className="des-box">Price : {price}</p>
+          <p className="des-box">offer : {description}</p>
+          {/* </div> */}
+          <button className="sign-up-btn pad " type="submit">
+            Accept Offer
+          </button>
+        </form>
+      </div>
     </>
   );
-}
+};
 
 export default OfferBox;
